4fa67a1e4fef2d466917c3cb1dfe3b5b3a7291db
MUFF1N1000 <tohtani1028@gmail.com>
2022-03-27 06:58:49 +0900

    様々な改善

--- discordbot.py
+++ discordbot.py
@@ -1,68 +1,58 @@
 import datetime
 from email import message
 from http import client
+from multiprocessing.connection import Client
 from tracemalloc import stop
 from unittest import async_case
 from dateutil.relativedelta import relativedelta
 import discord
 
-TOKEN = 'TOKEN'
-
+# discordのbotのtokenを入力
+TOKEN = 'hogehoge'
 client = discord.Client()
 
-# 日付の定義
-eventday1 = datetime.datetime(2022, 4, 1, 5, 0, 0)
-eventday2 = datetime.datetime(2022, 4, 16, 5, 0, 0)
-today = datetime.datetime.today()
-stopday = datetime.datetime(2050, 12, 31, 23, 59, 59)
-
-# 空のリストを作成
-outputdays1 = []
-outputdays2 = []
 
 # 起動時に実行
-
-
 @client.event
 async def on_ready():
-    # ターミナルにログインしたときに表示される
+    # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
-# 月の加算関数の定義
-
-
-def get_eventday(eventday):
-    return eventday + relativedelta(months=1)
-
-
-# 2050年12月31日までループし、イベントがある日付リストに追加する
-while 1:
-    calc = eventday1 - today
-    eventday1 = get_eventday(eventday1)
-    if 0 < calc.days < 16:  # 16日以内にイベントがある場合表示する
-        outputdays1.append(calc.days)
-    else:  # イベントが日付に達した場合ループを抜ける
-        break
-while 1:
-    calc = eventday2 - today
-    eventday2 = get_eventday(eventday2)
-    if 0 < calc.days < 16:  # 16日以内にイベントがある場合表示する
-        outputdays2.append(calc.days)
-    else:  # イベントが日付に達した場合ループを抜ける
-        break
+# メッセージ受信時に実行
 
 
 @client.event
 async def on_message(message):
+    # イベント日時を設定
+    today = datetime.datetime.today() + datetime.timedelta(hours=9)  # UCTからJSTに変換
+    eventday1 = datetime.datetime(2022, 3, 1, 5, 0, 0)
+    eventday2 = datetime.datetime(2022, 3, 16, 5, 0, 0)
+    stopday = datetime.datetime(2050, 12, 31, 23, 59, 59)
+    # 空の配列を作成
+    outputdays1 = []
+    outputdays2 = []
     if message.author.bot:
         return
     if message.content == '!rasen':
+        while 1:
+            eventday1 += relativedelta(months=1)  # イベント日時を1ヶ月増やす
+            calc = eventday1 - today
+            if 0 < calc.days < 16:  # 16日以内にイベントがある場合表示する
+                outputdays1.append(calc.days)
+            if eventday1 > stopday:  # イベント日時が終了日時を超えたらループを抜ける
+                break
+        while 1:
+            eventday2 += relativedelta(months=1)  # イベント日時を1ヶ月増やす
+            calc = eventday2 - today
+            if 0 < calc.days < 16:  # 16日以内にイベントがある場合表示する
+                outputdays2.append(calc.days)
+            if eventday2 > stopday:  # イベント日時が終了日時を超えたらループを抜ける
+                break
         if len(outputdays1) == 0:
             m1 = "螺旋終了まであと"+str(outputdays2[0])+"日"
             await message.channel.send(m1)
         else:
             m2 = "螺旋終了まであと"+str(outputdays1[0])+"日"
             await message.channel.send(m2)
-
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
